539e24e5ac912758b3efee3178b68ab37edb0052
JavierChacon <alejandro.chr@outlook.es>
2023-01-16 00:31:48 -0400

    fixed some details in the feed

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -6,14 +6,26 @@ import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoCha
 
 const VideoCard = ({ video: { id: { videoId }, snippet } }) => {
   return (
-    <Card>
+    <Card sx={{ width: { xs: '100%', sm: '358px', md: "320px", }, boxShadow: "none", borderRadius: '10px' }}>
       <Link to={videoId ? `video/${videoId}` : demoVideoUrl}>
         <CardMedia
           image={snippet?.thumbnails?.high?.url || demoThumbnailUrl}
           alt={snippet?.title}
-          sx={{width:358, height:180}}
-         />
-
+          sx={{ width: 358, height: 180 }}
+        />
+        <CardContent sx={{ backgroundColor: "#1E1E1E", height: '106px' }}>
+          <Link to={videoId ? `/video/${videoId}` : demoVideoUrl} >
+            <Typography variant="subtitle1" fontWeight="bold" color="#FFF">
+              {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
+            </Typography>
+          </Link>
+          <Link to={snippet?.channelId ? `/channel/${snippet?.channelId}` : demoChannelUrl} >
+            <Typography variant="subtitle2" color="gray">
+              {snippet?.channelTitle || demoChannelTitle}
+              <CheckCircle sx={{ fontSize: "12px", color: "gray", ml: "5px" }} />
+            </Typography>
+          </Link>
+        </CardContent>
       </Link>
     </Card>
   )
